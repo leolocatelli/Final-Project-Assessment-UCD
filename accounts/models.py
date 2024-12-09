dd6ee4e0b4678c999cfad00550c962ed8aa31ad7
Leo Locatelli <leolocatelli30@gmail.com>
2024-12-09 09:32:08 +0100

    user page initiation, save for rescue

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Você pode adicionar mais campos específicos ao usuário aqui
+    bio = models.TextField(null=True, blank=True)  # Exemplo: Biografia do usuário
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)  # Exemplo: Foto de perfil
+
+    def __str__(self):
+        return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Sua conta foi criada com sucesso! Faça login para continuar.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
