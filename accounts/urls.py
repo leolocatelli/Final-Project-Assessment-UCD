bd708d11bf71b0d731f19e39d808231ce4f7a23b
Leo Locatelli <leolocatelli30@gmail.com>
2024-12-23 09:21:09 +0100

    Password reset templates and functionality progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,24 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
+    # Página para solicitar reset de senha
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), 
+         name='password_reset'),
+         
+    # Página de confirmação após envio do e-mail
+    path('password_reset_done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), 
+         name='password_reset_done'),
+         
+    # Página para redefinir senha
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+         
+    # Página de sucesso após redefinição
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), 
+         name='password_reset_complete'),
 ]
 
